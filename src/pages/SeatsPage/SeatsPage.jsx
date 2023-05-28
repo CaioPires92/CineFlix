3523ad3d96123a9fbcc4f57b939efd20f7e1bc58
Caio Pires <caiogodoi_92@hotmail.com>
2023-05-27 23:18:05 -0300

    add footer

--- src/pages/SeatsPage/SeatsPage.jsx
+++ src/pages/SeatsPage/SeatsPage.jsx
@@ -86,16 +86,13 @@ export default function SeatsPage() {
       </FormContainer>
       <FooterContainer>
         <div>
-          <img
-            src={
-              'https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg'
-            }
-            alt="poster"
-          />
+          <img src={data.movie.posterURL} alt="poster" />
         </div>
         <div>
-          <p>Tudo em todo lugar ao mesmo tempo</p>
-          <p>Sexta - 14h00</p>
+          <p>{data.movie.title}</p>
+          <p>
+            {data.day.weekday} - {data.name}
+          </p>
         </div>
       </FooterContainer>
     </PageContainer>
